0afb7ed5ee24b0060964d1e00040f317bac4780d
lithdo <lithdo@gmail.com>
2014-02-20 20:44:31 +0800

    实现菜单项“打开”的功能

--- editor.py
+++ editor.py
@@ -71,7 +71,33 @@ class MdEditor(Gtk.Window):
         print("New file")
 
     def on_menu_file_open(self, widget):
-        print("Open file")
+        dialog = Gtk.FileChooserDialog("Open File...", self,
+            Gtk.FileChooserAction.OPEN,
+            (Gtk.STOCK_CANCEL,  Gtk.ResponseType.CANCEL,
+             Gtk.STOCK_OPEN,    Gtk.ResponseType.OK))
+
+        filter_text = Gtk.FileFilter()
+        filter_text.set_name("Text files")
+        filter_text.add_mime_type("text/plain")
+        dialog.add_filter(filter_text)
+
+        filter_py = Gtk.FileFilter()
+        filter_py.set_name("Python files")
+        filter_py.add_mime_type("text/x-python")
+        dialog.add_filter(filter_py)
+
+        filter_any = Gtk.FileFilter()
+        filter_any.set_name("Any files")
+        filter_any.add_pattern("*")
+        dialog.add_filter(filter_any)
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            print("File selected: " + dialog.get_filename())
+            fileopen = open(dialog.get_filename())
+            self.textbuffer.set_text(fileopen.read())
+
+        dialog.destroy()
 
     def on_menu_file_save(self, widget):
         print("Save file")
