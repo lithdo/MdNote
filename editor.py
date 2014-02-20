83bd9276979fad4b5e09b95493050935c750761f
lithdo <lithdo@gmail.com>
2014-02-20 19:41:27 +0800

    添加菜单

--- editor.py
+++ editor.py
@@ -2,27 +2,59 @@
 
 from gi.repository import Gtk
 
+UI_INFO = """
+<ui>
+    <menubar name='MenuBar'>
+        <menu action='FileMenu'>
+            <menuitem action='FileQuit' />
+        </menu>
+    </menubar>
+</ui>
+"""
+
 class MdEditor(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="MdNote")
-        
+
         self.set_default_size(800, 600)
         self.grid = Gtk.Grid()
         self.add(self.grid)
-        
+
+        action_group = Gtk.ActionGroup("my_actions")
+        self.add_file_menu_actions(action_group)
+
+        uimanager = Gtk.UIManager()
+        uimanager.add_ui_from_string(UI_INFO)
+        uimanager.insert_action_group(action_group)
+
+        menubar = uimanager.get_widget("/MenuBar")
+
+        self.grid.attach(menubar, 0, 0, 1, 1)
+
         self.create_textview()
-        
+
     def create_textview(self):
         scrolled_window = Gtk.ScrolledWindow()
         scrolled_window.set_hexpand(True)
         scrolled_window.set_vexpand(True)
-        self.grid.attach(scrolled_window, 0, 0, 1, 1)
-        
+        self.grid.attach(scrolled_window, 0, 1, 1, 1)
+
         self.textview = Gtk.TextView()
         self.textbuffer = self.textview.get_buffer()
         self.textview.set_wrap_mode(Gtk.WrapMode.WORD)
         scrolled_window.add(self.textview)
-        
+
+    def add_file_menu_actions(self, action_group):
+        action_filemenu = Gtk.Action("FileMenu", "File", None, None)
+        action_group.add_action(action_filemenu)
+
+        action_filequit = Gtk.Action("FileQuit", "Quit", None, None)
+        action_filequit.connect("activate", self.on_menu_file_quit)
+        action_group.add_action(action_filequit)
+
+    def on_menu_file_quit(self, widget):
+        Gtk.main_quit()
+
 editor = MdEditor()
 editor.connect("delete-event", Gtk.main_quit)
 editor.show_all()
