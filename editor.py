ec1098d1a1d89f8af30a747e6a45b66a701b8f30
lithdo <lithdo@gmail.com>
2014-02-21 20:14:20 +0800

    将菜单改为全局菜单（整合到Gnome-Shell顶部栏

--- editor.py
+++ editor.py
@@ -1,77 +1,70 @@
 #!/usr/bin/python3
 
 from gi.repository import Gtk, Gio
+import sys
 
-UI_INFO = """
-<ui>
-    <menubar name='MenuBar'>
-        <menu action='FileMenu'>
-            <menuitem action='FileNew' />
-            <menuitem action='FileOpen' />
-            <menuitem action='FileSave' />
-            <menuitem action='FileQuit' />
-        </menu>
-    </menubar>
-</ui>
-"""
-
-class MdEditor(Gtk.Window):
-    def __init__(self):
-        Gtk.Window.__init__(self, title="MdNote")
+class MdEditorWindow(Gtk.ApplicationWindow):
+    def __init__(self, app):
+        Gtk.Window.__init__(self, title="MdEditor", application=app)
 
         self.set_default_size(800, 600)
         self.grid = Gtk.Grid()
         self.add(self.grid)
 
-        action_group = Gtk.ActionGroup("my_actions")
-        self.add_file_menu_actions(action_group)
-
-        uimanager = Gtk.UIManager()
-        uimanager.add_ui_from_string(UI_INFO)
-        uimanager.insert_action_group(action_group)
-
-        menubar = uimanager.get_widget("/MenuBar")
-
-        self.grid.attach(menubar, 0, 0, 1, 1)
-
-        self.create_textview()
-
-    def create_textview(self):
         scrolled_window = Gtk.ScrolledWindow()
         scrolled_window.set_hexpand(True)
         scrolled_window.set_vexpand(True)
-        self.grid.attach(scrolled_window, 0, 1, 1, 1)
+        self.grid.attach(scrolled_window, 0, 0, 1, 1)
 
         self.textview = Gtk.TextView()
         self.textbuffer = self.textview.get_buffer()
         self.textview.set_wrap_mode(Gtk.WrapMode.WORD)
         scrolled_window.add(self.textview)
 
-    def add_file_menu_actions(self, action_group):
-        action_filemenu = Gtk.Action("FileMenu", "File", None, None)
-        action_group.add_action(action_filemenu)
+class MdEditor(Gtk.Application):
+    def __init__(self):
+        Gtk.Application.__init__(self)
 
-        action_filequit = Gtk.Action("FileNew", "New", None, None)
-        action_filequit.connect("activate", self.on_menu_file_new)
-        action_group.add_action(action_filequit)
+    def do_activate(self):
+        self.win = MdEditorWindow(self)
+        self.win.show_all()
 
-        action_filequit = Gtk.Action("FileOpen", "Open", None, None)
-        action_filequit.connect("activate", self.on_menu_file_open)
-        action_group.add_action(action_filequit)
+    def do_startup(self):
+        Gtk.Application.do_startup(self)
 
-        action_filequit = Gtk.Action("FileSave", "Save", None, None)
-        action_filequit.connect("activate", self.on_menu_file_save)
-        action_group.add_action(action_filequit)
+        menu = Gio.Menu()
+        menu.append("New", "app.newfile")
+        menu.append("Open", "app.openfile")
+        menu.append("Save", "app.savefile")
 
-        action_filequit = Gtk.Action("FileQuit", "Quit", None, None)
-        action_filequit.connect("activate", self.on_menu_file_quit)
-        action_group.add_action(action_filequit)
+        quit_menu = Gio.Menu()
+        quit_menu.append("Quit", "app.quit")
 
-    def on_menu_file_new(self, widget):
+        menu.append_section("",quit_menu)
+
+        self.set_app_menu(menu)
+
+        newfile_action = Gio.SimpleAction.new("newfile", None)
+        newfile_action.connect("activate", self.on_menu_file_new)
+        self.add_action(newfile_action)
+
+        openfile_action = Gio.SimpleAction.new("openfile", None)
+        openfile_action.connect("activate", self.on_menu_file_open)
+        self.add_action(openfile_action)
+
+        savefile_action = Gio.SimpleAction.new("savefile", None)
+        savefile_action.connect("activate", self.on_menu_file_save)
+        self.add_action(savefile_action)
+
+        quit_action = Gio.SimpleAction.new("quit", None)
+        quit_action.connect("activate", self.on_menu_file_quit)
+        self.add_action(quit_action)
+
+    def on_menu_file_new(self, action, parameter):
         print("New file")
 
-    def on_menu_file_open(self, widget):
-        dialog = Gtk.FileChooserDialog("Open File...", self,
+    def on_menu_file_open(self, action, parameter):
+        dialog = Gtk.FileChooserDialog("Open File...", self.win,
             Gtk.FileChooserAction.OPEN,
             (Gtk.STOCK_CANCEL,  Gtk.ResponseType.CANCEL,
              Gtk.STOCK_OPEN,    Gtk.ResponseType.OK))
@@ -95,17 +88,16 @@ class MdEditor(Gtk.Window):
         if response == Gtk.ResponseType.OK:
             print("File selected: " + dialog.get_filename())
             fileopen = open(dialog.get_filename())
-            self.textbuffer.set_text(fileopen.read())
+            self.win.textbuffer.set_text(fileopen.read())
 
         dialog.destroy()
 
-    def on_menu_file_save(self, widget):
+    def on_menu_file_save(self, action, parameter):
         print("Save file")
 
-    def on_menu_file_quit(self, widget):
-        Gtk.main_quit()
-
-editor = MdEditor()
-editor.connect("delete-event", Gtk.main_quit)
-editor.show_all()
-Gtk.main()
+    def on_menu_file_quit(self, action, parameter):
+        self.quit()
+            
+app = MdEditor()
+exit_status = app.run(sys.argv)
+sys.exit(exit_status)
